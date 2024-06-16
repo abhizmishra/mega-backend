11558eead69ec2e920c6a5eb8bd5779336012394
abhizmishra <sonusamda@gmail.com>
2024-06-16 16:37:21 +0530

    //get user details from frontend   //validation - not empty   //check if user already exists: username,email   //check for images,check for avatar   //upload them to cloudinary ,avatar   //create user object - create entry in db   //remove password and refresh token field from response   //check for user creation   //return res

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { regiterUser } from "../controllers/user.controller.js";
+import { registerUser } from "../controllers/user.controller.js";
 
 
 const router = Router()
 
-router.route("/register").post(regiterUser)
+router.route("/register").post(registerUser);
 
 export default router
