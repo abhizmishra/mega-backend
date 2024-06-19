0661d649372e78c75f80029398f5d9379612b603
abhizmishra <sonusamda@gmail.com>
2024-06-19 10:51:34 +0530

    Logoutuser section added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -126,7 +126,7 @@ const loginUser = asyncHandler(async (req, res) => {
     secure:true
   }
 
-  
+
 
   return res.status(200)
     .cookie("accessToken",accessToken,options)
@@ -141,5 +141,27 @@ const loginUser = asyncHandler(async (req, res) => {
     )
   )
 });
+
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id, {
+      $unset: {
+        refreshToken:1
+      }
+  },
+    {
+      new:true
+    }
+  )
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+  .json(new ApiResponse(200,{},"User logged Out"))
+})
 
 export { registerUser, loginUser };
