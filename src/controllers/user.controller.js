e0872cca38454b8837f8f4609b6692e4a2fc8df8
abhizmishra <sonusamda@gmail.com>
2024-06-27 10:19:55 +0530

    .

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -219,7 +219,9 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
   user.password = newPassword 
   await user.save({ validateBeforeSave: false })
   
-  return res.status(200).json(new ApiResponse(200,{},"Passwor changed successfully"))
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully"));
 });
 
 
